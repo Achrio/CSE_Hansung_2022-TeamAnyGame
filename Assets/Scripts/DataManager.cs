f4709dc5b4b99e347dfc627fb3ad7e03e4efa5d6
Achrio <achro98@naver.com>
2022-06-01 16:29:25 +0900

    UI 수정

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -7,9 +7,7 @@ using System.Linq;
 //saveData form
 [System.Serializable]
 public class SaveData {
-    public int money;
     public int HP;
-    public int dash;
     public List<float> clearTime;
 }
 
@@ -39,30 +37,16 @@ public class DataManager : MonoBehaviour {
             saveData = JsonUtility.FromJson<SaveData>(loadData);
 
             if(saveData != null) {
-                GameManager.instance.money = saveData.money;
-                GameManager.instance.HP = saveData.HP;
-                GameManager.instance.dash = saveData.dash;
-
                 //Add amount of stages
                 GameManager.instance.clearTime[0] = saveData.clearTime[0];
                 GameManager.instance.clearTime[1] = saveData.clearTime[1];
-                GameManager.instance.clearTime[2] = saveData.clearTime[2];
-                GameManager.instance.clearTime[3] = saveData.clearTime[3];
             }
         }
-
-        //update all UI
-        GameManager.instance.curHPUpdate(0);
-        GameManager.instance.MoneyUpdate(0);
-        //dashUI.Dash.initDashCount(GameManager.instance.dash); //init dash count in dashUI
     }
 
     public void DataSave() {
         SaveData saveData = new SaveData();
 
-        saveData.money = GameManager.instance.money;
-        saveData.HP = GameManager.instance.HP;
-        saveData.dash = GameManager.instance.dash;
         saveData.clearTime = GameManager.instance.clearTime;
 
         string json = JsonUtility.ToJson(saveData, true);
@@ -75,13 +59,8 @@ public class DataManager : MonoBehaviour {
     }
 
     public void DataNew() {
-        GameManager.instance.money = 0;
-        GameManager.instance.HP = 5;
-        GameManager.instance.dash = 3;
         GameManager.instance.clearTime[0] = 0f;
         GameManager.instance.clearTime[1] = 0f;
-        GameManager.instance.clearTime[2] = 0f;
-        GameManager.instance.clearTime[3] = 0f;
 
         DataSave();
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,39 +13,18 @@ public class GameManager : MonoBehaviour {
     public List<string> stageName; //stageName (add in Inspector)
     
     [Header ("Values")]
-    public int money;
     public int HP;
-    public int dash;
     public List<float> clearTime;
 
     void Awake() {
         instance = this;
         clearTime = new List<float>();
-        clearTime.Add(0f);
-        clearTime.Add(0f);
-        clearTime.Add(0f);
-        clearTime.Add(0f);
+        for(int i = 0; i< stageName.Count; i++) {
+            clearTime.Add(0f);
+        }
+        GameManager.instance.curHPUpdate(0);
     }
 
-    void Start() {
-        /* 대쉬 UI 임시
-        cshTimer timer = this.gameObject.GetComponent<cshTimer>();
-        timer.dashCount = this.maxDash; //init cshTimer dashCount
-        timer.count = this.curDash; //init cshTimer dashCount
-        */
-    }
-
-    //Game Status Change
-    void Update() {
-        
-    }
-
-    //UI Update (Called by each object's func)
-    public void MoneyUpdate(int changeValue) {
-        money += changeValue;
-
-        moneyUI.Money.moneyUpdate(money);
-    }
     public void curHPUpdate(int changeValue) {
         HP += changeValue;
 
