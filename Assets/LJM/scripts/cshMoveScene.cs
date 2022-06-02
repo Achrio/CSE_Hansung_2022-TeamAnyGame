24329328215d8f654c66a67814ecece5004f73a4
Achrio <achro98@naver.com>
2022-06-02 15:24:21 +0900

    포탈 상호작용 추가
    포탈 열려있지 않은 경우 아예 보여지지 않도록 함
    
    메트로배니아 스테이지 포탈 : 기본 생성되어 있음
    러닝액션 스테이지 포탈 : 메트로배니아 스테이지 클리어 시간 확인하여 오픈
    보스 스테이지 포탈 : 러닝액션 스테이지 클리어 시간 확인하여 오픈

--- Assets/LJM/scripts/cshMoveScene.cs
+++ Assets/LJM/scripts/cshMoveScene.cs
@@ -15,10 +15,37 @@ public class cshMoveScene : MonoBehaviour
         audioSource.clip = PortalAudio;
     }
 
+    public GameObject portal;
+
+    //스테이지별 포탈 프리팹마다 인덱스 지정
+    //metro 포탈 stageNum = 0
+    //running action 포탈 stageNum = 1
+    //boss 포탈 stageNum = 2
+    public int stageNum = 0;
+
+    //전부다 미리 열어두고 싶은 경우 inspector에서 isOn true로 체크
+    public bool isOn = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(!isOn) {
+            switch(stageNum) {
+                case 0 : //metro 포탈 : 기본으로 오픈
+                    isOn = true;
+                    break;
+                case 1 : //running action 포탈 : metro 클리어시간 확인 후 오픈
+                    if(GameManager.instance.clearTime[0] != 0.0f) isOn = true;
+                    break;
+                case 2 : //boss 포탈 : running action 클리어시간 확인 후 오픈
+                    if(GameManager.instance.clearTime[1] != 0.0f) isOn = true;
+                    break;
+            }
+        }
+
+        if(!isOn) {
+            portal.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -29,16 +56,17 @@ public class cshMoveScene : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z) && isOn)
         {
             audioSource.Play();
             SceneManager.LoadScene(SceneName);
+            SceneLoadingManager.LoadScene(SceneName);
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z) && isOn)
         {
             audioSource.Play();
             SceneLoadingManager.LoadScene(SceneName);
@@ -47,7 +75,7 @@ public class cshMoveScene : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z) && isOn)
         {
             audioSource.Play();
             SceneLoadingManager.LoadScene(SceneName);
